9b4e4392d4928a9565b8802805af277fe52356ee
Derek Fleming <derekd91.df@gmail.com>
2022-05-07 18:58:23 +1000

    Spring init

---
+++ src/main/java/com/derekfleming/sportsbetchallenge/SportsbetChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.derekfleming.sportsbetchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsbetChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportsbetChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/derekfleming/sportsbetchallenge/SportsbetChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.derekfleming.sportsbetchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportsbetChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
